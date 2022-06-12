b5732293c372b7a7a82da3df2e542def448e1c05
Alabama Whiskey <amwtablet@gmail.com>
2022-06-12 10:08:14 -0400

    progress on Smallest Integral Divisor

---
+++ 03-lisp-to-js/smallestDivisor.js
@@ -0,0 +1,23 @@
+//Finds the smallest integral divisor (greater than 1) of a given number n
+    // (define (smallest-divisor n)
+    //   (find-divisor n 2))
+    // (define (find-divisor n test-divisor)
+    //   (cond ((> (square test-divisor) n) n)
+    //         ((divides? test-divisor n) test-divisor)
+    //         (else (find-divisor n (+ test-divisor 1)))))
+    // (define (divides? a b)
+    //   (= (remainder b a) 0))
+
+
+//JavaScript Translation
+const smallestDivisor = (n) => {
+
+};
+
+const findDivisor = (n, testDivisor) => {
+    if ((testDivisor^2) > n)
+};
+
+const checkDivisible = (a, b) => {
+
+};
